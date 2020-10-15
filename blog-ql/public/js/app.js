8d9c55c3c9dfa67fb99343de59d91cbdc5c99cc9
vankatwijk <hendrikus.hpvk@gmail.com>
2020-10-15 18:31:51 +0200

    init vue

--- blog-ql/public/js/app.js
+++ blog-ql/public/js/app.js
@@ -49879,26 +49879,14 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./resources/sass/app.scss":
-/*!*********************************!*\
-  !*** ./resources/sass/app.scss ***!
-  \*********************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-
 /***/ 0:
-/*!*************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/css/app.css ***!
-  \*************************************************************************************/
+/*!***********************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/css/app.css ***!
+  \***********************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /home/hkatwijk/repo/BlogQL-Laravel-Vue-GraphQL-Tailwind/blog-ql/resources/js/app.js */"./resources/js/app.js");
-__webpack_require__(/*! /home/hkatwijk/repo/BlogQL-Laravel-Vue-GraphQL-Tailwind/blog-ql/resources/sass/app.scss */"./resources/sass/app.scss");
 module.exports = __webpack_require__(/*! /home/hkatwijk/repo/BlogQL-Laravel-Vue-GraphQL-Tailwind/blog-ql/resources/css/app.css */"./resources/css/app.css");
 
 
--- blog-ql/webpack.mix.js
+++ blog-ql/webpack.mix.js
@@ -16,4 +16,8 @@ mix.postCss('resources/css/app.css', 'public/css', [
 
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+
+if(mix.inProduction()){
+
+    mix.version()
+}
