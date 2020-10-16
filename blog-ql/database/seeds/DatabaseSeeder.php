bcd7a98a7a278f5eef734b970a0b211c45945628
vankatwijk <hendrikus.hpvk@gmail.com>
2020-10-16 12:36:19 +0200

    factory/seeder for post

--- blog-ql/database/seeds/DatabaseSeeder.php
+++ blog-ql/database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Post;
 use App\User;
 use App\Topic;
 use Illuminate\Database\Seeder;
@@ -19,5 +20,9 @@ class DatabaseSeeder extends Seeder
         Topic::create(['name' => 'Featured Sites', 'slug' => 'featured']);
         Topic::create(['name' => 'Useful Links', 'slug' => 'links']);
         Topic::create(['name' => 'Guides & Tutorials', 'slug' => 'tutorials']);
+
+
+        factory(Post::class, 20)->create();
+
     }
 }
